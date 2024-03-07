994a4fa4d730d8763e34e1aa2b93d5726f12346a
Rahul Kumar <rahulkumar5522550@gmail.com>
2024-03-07 12:50:02 +0530

    Removed Accidental whiteSpaces

--- SalonSphere-Server/src/main/java/com/SalonSphereServer/Service/UserService.java
+++ SalonSphere-Server/src/main/java/com/SalonSphereServer/Service/UserService.java
@@ -27,7 +27,6 @@ public class UserService {
 		// database so we
 		if (findUser == null) {
 
-			
 			// Write code for validation
 			if (Validation.emailValidation(user.getEmail())
 					&& Validation.contactNumberValidation(user.getContactNumber())
@@ -38,17 +37,21 @@ public class UserService {
 				user.setUserId(UUID.randomUUID().toString());
 				user.setIsDeleted(false);
 				user.setPassword(passwordEncoder.encode(user.getPassword()));
-				
-				
-				// Create a java.util.Date object
-		        java.util.Date utilDate = new java.util.Date();
 
-		        // Convert java.util.Date to java.sql.Date
-		        java.sql.Date sqlDate = new java.sql.Date(utilDate.getTime());
+				// Create a java.util.Date object
+				java.util.Date utilDate = new java.util.Date();
+				System.out.println("printinfg Date=============");
+
+				// Convert java.util.Date to java.sql.Date
+				java.sql.Date sqlDate = new java.sql.Date(utilDate.getTime());
 				
+				
+				System.out.println("printinfg Date="+sqlDate);
+
 				user.setCreatedDate(sqlDate);
 				user.setModifyDate(sqlDate);
 
+				System.out.println("printinfg Date=============");
 				// Save in the database
 				findUser = userRepository.save(user);
 				return true;
