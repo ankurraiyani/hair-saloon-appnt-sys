739e714b9ad134e02ee946903143d1dcd40a657b
Aman Gupta <guptaaman6264@gmail.com>
2024-03-07 12:36:15 +0530

    modify packages name

--- SalonSphere-Server/src/main/java/com/SalonSphereServer/entity/Users.java
+++ SalonSphere-Server/src/main/java/com/SalonSphereServer/entity/Users.java
@@ -1,4 +1,4 @@
-package com.SalonSphereServer.Entity;
+package com.SalonSphereServer.entity;
 
 
 import java.util.Date;
--- SalonSphere-Server/src/main/java/com/SalonSphereServer/service/UserService.java
+++ SalonSphere-Server/src/main/java/com/SalonSphereServer/service/UserService.java
@@ -6,7 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
-import com.SalonSphereServer.Entity.Users;
+import com.SalonSphereServer.entity.Users;
 import com.SalonSphereServer.repository.UserRepository;
 import com.SalonSphereServer.common.Validation;
 
