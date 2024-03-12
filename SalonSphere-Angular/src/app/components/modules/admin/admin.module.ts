552740b03013d357c47f1d4fd8cf11251ea3ba2d
ajay15codes <dawandey15aj@gmail.com>
2024-03-12 18:15:48 +0530

    Merging

--- SalonSphere-Angular/src/app/components/modules/admin/admin.module.ts
+++ SalonSphere-Angular/src/app/components/modules/admin/admin.module.ts
@@ -1,11 +1,11 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
 
 import { AdminRoutingModule } from './admin-routing.module';
 import { AdminDashboardComponent } from './components/admin-dashboard/admin-dashboard.component';
+import { HomeComponent } from './components/home/home.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { ViewRequestComponent } from './components/view-request/view-request.component';
-import { HomeComponent } from './components/home/home.component';
 import { ViewUserComponent } from './components/view-user/view-user.component';
 
 
