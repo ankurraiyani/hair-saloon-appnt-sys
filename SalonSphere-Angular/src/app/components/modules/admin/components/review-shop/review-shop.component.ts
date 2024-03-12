4742b50ec76a57848da6190a40803c234eb84084
Aman Gupta <guptaaman6264@gmail.com>
2024-03-12 16:37:22 +0530

    add Approve and Reject shop services

--- SalonSphere-Angular/src/app/components/modules/admin/components/review-shop/review-shop.component.ts
+++ SalonSphere-Angular/src/app/components/modules/admin/components/review-shop/review-shop.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { ShopReviewService } from '../../../../services/shop-review/shop-review.service';
+import { Router } from '@angular/router';
+import { response } from 'express';
+import Swal from 'sweetalert2';
 
 interface shopInformation{
   ownerName:string,
@@ -20,7 +24,7 @@ interface shopInformation{
 })
 export class ReviewShopComponent implements OnInit{
 
-  constructor(private route: ActivatedRoute){}
+  constructor(private route: ActivatedRoute,private shopReviewService: ShopReviewService, private router:Router){}
 
   public shops: shopInformation []= [{
     ownerName: "Aman Gupta",
@@ -44,7 +48,69 @@ export class ReviewShopComponent implements OnInit{
   showAllShopInformation(){
     console.log("come inside the show method");
     
+    //git shop Email from the localStorage 
+    const shopEmail = localStorage.getItem('shopEmail');
+
+    //if shopEmail is not present in the localStorage then redirect to the view-request page
+    if (shopEmail === null) {
+      this.router.navigate(['/admin/view-request']);
+      return ;
+    }
+
+    //else call the service which  will fetch shop information using the shopEmail
+    this.shopReviewService.getReviewShop(shopEmail).subscribe((response:any)=>{
+      console.log(response);
+      this.shops= response;
+    },
+    error=>{
+      console.log("error occured"+ error);
+    });
   }
 
+  //approve the shop
+  public approveShop(){
+      console.log("come inside the approve");
+
+      //call the service
+      this.shopReviewService.approveRequest().subscribe(response=>{
+        console.log(response);
+        Swal.fire({
+          title: 'Good Job!',
+          text: 'Approved Successfully',
+          icon: 'success',
+        });
+      },
+      error=>{
+        console.log("error occured"+error);
+        Swal.fire({
+          title: 'Error',
+          text: 'Server error occured',
+          icon: 'error',
+        });
+      })
+  }
+
+  //reject the shop
+  public rejectShop(){
+    console.log("come inside the reject");
+    
+    //call the service
+    this.shopReviewService.rejectRequest().subscribe(response=>{
+      console.log(response);
+      Swal.fire({
+        title: 'Good Job!',
+        text: 'Rejected Successfully',
+        icon: 'success',
+      });
+    },
+    error=>{
+      console.log("error occured"+error);
+      Swal.fire({
+        title: 'Error',
+        text: 'Server error occured',
+        icon: 'error',
+      });
+    })
+  }
 
 }
--- SalonSphere-Angular/src/app/components/services/shop-review/shop-review.service.ts
+++ SalonSphere-Angular/src/app/components/services/shop-review/shop-review.service.ts
@@ -1,9 +1,60 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Cookie } from 'ng2-cookies';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ShopReviewService {
+  constructor(private httpClient: HttpClient) {}
 
-  constructor() { }
+  baseURL: string = 'http://localhost:8081/admin/view-requests/review-shop';
+
+  //get the shop which is being Review by the admin
+  public getReviewShop(shopEmailId: string) {
+    console.log('come inside the service');
+
+    //set the token in header
+    const token = Cookie.get('token');
+    const headers = new HttpHeaders().set(
+      'Authorization',
+      'Bearer ' + token
+    );
+    return this.httpClient.post(`${this.baseURL}`, shopEmailId, { headers });
+
+  }
+
+  //URL for Approval
+  approveURL: string = 'http://localhost:8081/admin/view-requests/approve-request';
+
+  //approve the shop which i begin review by the admin
+  public approveRequest() {
+    console.log('come inside approve-requests');
+
+    //set the token in header
+    const token = Cookie.get('token');
+    const headers = new HttpHeaders().set(
+      'Authorization',
+      'Bearer ' + token
+    );
+    return this.httpClient.get(`${this.approveURL}`, { headers });
+  }
+
+  //URL for Rejection
+  rejectURL: string = 'http://localhost:8081/admin/view-requests/reject-request';
+
+  //approve the shop which i begin review by the admin
+  public rejectRequest() {
+    console.log('come inside approve-requests');
+
+    //set the token in header
+    const token = Cookie.get('token');
+    const headers = new HttpHeaders().set(
+      'Authorization',
+      'Bearer ' + token
+    );
+    return this.httpClient.get(`${this.rejectURL}`, { headers });
+  }
+
+
 }
