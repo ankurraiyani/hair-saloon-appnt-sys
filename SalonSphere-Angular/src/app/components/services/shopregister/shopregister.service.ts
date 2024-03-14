0353ed33bf3eabd394923521823e14826c764b23
Krunal <krunal7022110@gmail.com>
2024-03-14 15:18:43 +0530

    some changes

--- SalonSphere-Angular/src/app/components/services/shopregister/shopregister.service.ts
+++ SalonSphere-Angular/src/app/components/services/shopregister/shopregister.service.ts
@@ -9,23 +9,11 @@ export class ShopregisterService {
   constructor(private http: HttpClient) {}
 
   registerShop(data: any) {
-    const headers = new HttpHeaders().set(
-      'Authorization',
-      'Bearer ' + Cookie.get('token')
-    );
-
-<<<<<<< HEAD
-    console.log('Yeh hai headder', headers);
-    return this.http.post('http://localhost:8081/shopkeeper/addshop', data, {
-      headers,
-    });
-=======
-  registerShop(data:any){
+  
     const headers = new HttpHeaders().set('Authorization', 'Bearer ' + Cookie.get('token'));
 
     console.log("Yeh hai headder",headers);
     return this.http.post('http://localhost:8081/shopkeeper/addshop', data, {headers} );
     // return this.http.post('http://localhost:8081/shopkeeper/addshop',data);
->>>>>>> 2ff445820f999c6fa303afeaf2cdfb7dee72ace7
   }
 }
