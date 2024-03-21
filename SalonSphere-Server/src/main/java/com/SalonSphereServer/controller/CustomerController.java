13df8016e14db26cfb882f68d15ac6b477d1bd27
Rahul Kumar <rahulkumar5522550@gmail.com>
2024-03-21 19:09:28 +0530

    Creating Feedback APIs

--- SalonSphere-Server/src/main/java/com/SalonSphereServer/controller/CustomerController.java
+++ SalonSphere-Server/src/main/java/com/SalonSphereServer/controller/CustomerController.java
@@ -12,21 +12,28 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.SalonSphereServer.entity.Feedback;
+import com.SalonSphereServer.repository.FeedbackRepository;
 import com.SalonSphereServer.request.FilterRequest;
 import com.SalonSphereServer.response.FilterResponse;
 import com.SalonSphereServer.response.FilterResponseByCity;
+import com.SalonSphereServer.response.Response;
 import com.SalonSphereServer.service.CustomerService;
+import com.SalonSphereServer.service.FeedbackService;
 
 // This is Shopkeerper related  controller class  for handling shopkeeper related API
 @RestController
 @CrossOrigin(origins = "http://localhost:4200")
-//@RequestMapping("/customer")
+// @RequestMapping("/customer")
 public class CustomerController {
 
 	@Autowired
 	private CustomerService customerService;
-	
-	
+	@Autowired
+	private FeedbackService feedbackService;
+	@Autowired
+	private FeedbackRepository feedbackRepository;
+
 	// ============================================================================================================
 	// Filter shops by given city
 	@CrossOrigin(origins = "http://localhost:4200")
@@ -96,6 +103,68 @@ public class CustomerController {
 		}
 	}
 
+	// ================CODE FOR FEEDBACK/REVIEW/RATING===========================
+	// Through this method the user can give feedback to the provider
+	@CrossOrigin(origins = "http://localhost:4200")
+	@PostMapping("/feedback")
+	public ResponseEntity<Response> addFeedBack(@RequestBody Feedback feedback) {
+
+		System.out.println("=====INSIDE THE COUSTOMERCONTROLLER ADDFEEDBACK======\n" + feedback);
+		boolean isAdd = feedbackService.addFeedBack(feedback);
+		if (isAdd)
+			return ResponseEntity.ok().body(new Response("Review added Successfully"));
+		else
+			return ResponseEntity.badRequest().body(new Response("Review not added"));
+	}
+
+	// Through this controller we get all leatest feedback in deasending order by
+	// date from the database by shopid
+	@CrossOrigin(origins = "http://localhost:4200")
+	@PostMapping("/get-all-feedback/{shopId}")
+	public ResponseEntity<List<Feedback>> getAllFeedbackByShopId(@PathVariable String shopId) {
+
+		System.out.println("=====INSIDE THE COUSTOMERCONTROLLER getAllFeedbackByShopId======\n" + shopId);
+		List<Feedback> fList = feedbackRepository.findByShopIdOrderByReviewDateDesc(shopId);
+		if (!fList.isEmpty())
+			return ResponseEntity.ok().body(fList);
+		else
+			return ResponseEntity.badRequest().body(fList);
+	}
+
+	// This API's is used for getting values of likes by review_id
+	@CrossOrigin(origins = "http://localhost:4200")
+	@PostMapping("/likes-by-reviewid/{reviewId}")
+	public ResponseEntity<Response> getLikesByReviewId(@PathVariable int reviewId) {
+		System.out.println("=====INSIDE THE COUSTOMERCONTROLLER getLikesByReviewId======\n" + reviewId);
+		return ResponseEntity.ok().body(new Response("" + feedbackRepository.findLikesByReviewId(reviewId)));
+	}
+
+	// This API's is used for updating likes value by 1 with the help of review_id
+	@CrossOrigin(origins = "http://localhost:4200")
+	@PostMapping("/like-by-ince")
+	public ResponseEntity<Response> incrementLikeByReviewId(@PathVariable int reviewId, @PathVariable int like) {
+		System.out.println(
+				"=====INSIDE THE COUSTOMERCONTROLLER  incrementLikeByReviewId======\n" + reviewId + "," + like);
+		feedbackRepository.updateLikesByReviewId(like, reviewId);
+		return ResponseEntity.ok().body(new Response("Like Increment By 1."));
+	}
+
+	// This API's is used for updating likes value by -1 with the help of review_id
+	@CrossOrigin(origins = "http://localhost:4200")
+	@PostMapping("/like-by-desc")
+	public ResponseEntity<Response> decrementLikeByReviewId(@PathVariable int reviewId, @PathVariable int like) {
+		System.out.println(
+				"=====INSIDE THE COUSTOMERCONTROLLER  incrementLikeByReviewId======\n" + reviewId + "," + like);
+		feedbackRepository.updateLikesByReviewId(like, reviewId);
+		return ResponseEntity.ok().body(new Response("Like decrement By -1."));
+	}
+
+	// This API's is used for updating likes value by -1 with the help of review_id
+	@CrossOrigin(origins = "http://localhost:4200")
+	@PostMapping("/delete-review/{reviewId}")
+	public ResponseEntity<Response> deleteReviewId(@PathVariable int reviewId) {
+		System.out.println("=====INSIDE THE COUSTOMERCONTROLLER  deleteReviewId======\n" + reviewId);
+		feedbackRepository.deleteById(reviewId);
+		return ResponseEntity.ok().body(new Response("Review Deleted Succeefully"));
+	}
 }
-
-
